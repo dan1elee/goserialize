2de13119e79c9822c775f6f6fa950a5b8c30f299
dan1elee <1398815412@qq.com>
2024-07-20 17:17:38 +0800

    完成数组、切片和字符串的序列化，优化了Encode函数的输入

--- encoder/encode.go
+++ encoder/encode.go
@@ -7,17 +7,9 @@ import (
 	"reflect"
 )
 
-func Encode(v interface{}) ([]byte, error) {
-	refVal := reflect.ValueOf(v)
+func Encode(refVal reflect.Value) ([]byte, error) {
 	kind := refVal.Kind()
 	switch kind {
-	case reflect.Invalid:
-		if v == nil {
-			return []byte{enums.NIL, 2}, nil
-		} else {
-			return nil, errors.New("invalid value")
-		}
-
 	case reflect.Bool:
 		return encodeBool(refVal)
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
@@ -145,8 +137,27 @@ func encodeComplex(v reflect.Value) (bytes []byte, err error) {
 }
 
 func encodeArray(v reflect.Value) (bytes []byte, err error) {
-	// todo
-	return nil, errors.New("TODO")
+	length := v.Len()
+	if length <= 0 {
+		return []byte{enums.ARRAY, 2}, nil
+	} else if length > 255 {
+		return nil, errors.New("max length exceeded")
+	}
+	var content []byte
+	var contentLength int = 0
+	for i := 0; i < length; i++ {
+		valI := v.Index(i)
+		ret, err := Encode(valI)
+		if err != nil {
+			return nil, err
+		}
+		content = append(content, ret...)
+		contentLength += len(content)
+		if contentLength > 252 {
+			return nil, errors.New("max length exceeded")
+		}
+	}
+	return append([]byte{enums.ARRAY, byte(contentLength + 3), byte(length)}, content...), nil
 }
 
 func encodeStruct(v reflect.Value) (bytes []byte, err error) {
@@ -155,13 +166,33 @@ func encodeStruct(v reflect.Value) (bytes []byte, err error) {
 }
 
 func encodeString(v reflect.Value) (bytes []byte, err error) {
-	// todo
-	return nil, errors.New("TODO")
+	val := v.String()
+	val2Bytes := []byte(val)
+	return append([]byte{enums.STRING, byte(len(val2Bytes) + 2)}, val2Bytes...), nil
 }
 
 func encodeSlice(v reflect.Value) (bytes []byte, err error) {
-	// todo
-	return nil, errors.New("TODO")
+	length := v.Len()
+	if length <= 0 {
+		return []byte{enums.SLICE, 2}, nil
+	} else if length > 255 {
+		return nil, errors.New("max length exceeded")
+	}
+	var content []byte
+	var contentLength int = 0
+	for i := 0; i < length; i++ {
+		valI := v.Index(i)
+		ret, err := Encode(valI)
+		if err != nil {
+			return nil, err
+		}
+		content = append(content, ret...)
+		contentLength += len(content)
+		if contentLength > 252 {
+			return nil, errors.New("max length exceeded")
+		}
+	}
+	return append([]byte{enums.SLICE, byte(contentLength + 3), byte(length)}, content...), nil
 }
 
 func encodeMap(v reflect.Value) (bytes []byte, err error) {
