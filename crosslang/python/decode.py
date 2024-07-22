8ab82f3992215bbca19b3656d495c27f919db800
dan1elee <87975928+dan1elee@users.noreply.github.com>
2024-07-22 10:23:25 +0800

    增加python反序列化Struct的实现，修正一些代码风格

--- crosslang/python/decode.py
+++ crosslang/python/decode.py
@@ -38,43 +38,45 @@ def decode(data: bytes):
     elif data[1] != length:
         raise WrongFormException(WrongFormErrorType.LengthNotEqual)
     if data[0] == Type.BOOL.value:
-        return struct.unpack('<?', data[2:])[0]
+        return struct.unpack("<?", data[2:])[0]
     elif data[0] == Type.INT.value:
-        return struct.unpack('<q', data[2:])[0]
+        return struct.unpack("<q", data[2:])[0]
     elif data[0] == Type.INT8.value:
-        return struct.unpack('<b', data[2:])[0]
+        return struct.unpack("<b", data[2:])[0]
     elif data[0] == Type.INT16.value:
-        return struct.unpack('<h', data[2:])[0]
+        return struct.unpack("<h", data[2:])[0]
     elif data[0] == Type.INT32.value:
-        return struct.unpack('<i', data[2:])[0]
+        return struct.unpack("<i", data[2:])[0]
     elif data[0] == Type.INT64.value:
-        return struct.unpack('<q', data[2:])[0]
+        return struct.unpack("<q", data[2:])[0]
     elif data[0] == Type.UINT.value:
-        return struct.unpack('<Q', data[2:])[0]
+        return struct.unpack("<Q", data[2:])[0]
     elif data[0] == Type.UINT8.value:
-        return struct.unpack('<B', data[2:])[0]
+        return struct.unpack("<B", data[2:])[0]
     elif data[0] == Type.UINT16.value:
-        return struct.unpack('<H', data[2:])[0]
+        return struct.unpack("<H", data[2:])[0]
     elif data[0] == Type.UINT32.value:
-        return struct.unpack('<I', data[2:])[0]
+        return struct.unpack("<I", data[2:])[0]
     elif data[0] == Type.UINT64.value:
-        return struct.unpack('<Q', data[2:])[0]
+        return struct.unpack("<Q", data[2:])[0]
     elif data[0] == Type.FLOAT32.value:
-        return struct.unpack('<f', data[2:])[0]
+        return struct.unpack("<f", data[2:])[0]
     elif data[0] == Type.FLOAT64.value:
-        return struct.unpack('<d', data[2:])[0]
+        return struct.unpack("<d", data[2:])[0]
     elif data[0] == Type.COMPLEX64.value:
-        real = struct.unpack('<f', data[2:6])[0]
-        imag = struct.unpack('<f', data[6:10])[0]
+        real = struct.unpack("<f", data[2:6])[0]
+        imag = struct.unpack("<f", data[6:10])[0]
         return complex(real, imag)
     elif data[0] == Type.COMPLEX128.value:
-        real = struct.unpack('<d', data[2:10])[0]
-        imag = struct.unpack('<d', data[10:18])[0]
+        real = struct.unpack("<d", data[2:10])[0]
+        imag = struct.unpack("<d", data[10:18])[0]
         return complex(real, imag)
     elif data[0] == Type.ARRAY.value:
         return decodeArray(data)
+    elif data[0] == Type.STRUCT.value:
+        return decodeStruct(data)
     elif data[0] == Type.STRING.value:
-        return data[2:].decode('utf-8')
+        return data[2:].decode("utf-8")
     elif data[0] == Type.SLICE.value:
         return decodeSlice(data)
     else:
@@ -88,16 +90,33 @@ def decodeArray(data: bytes) -> List[Any]:
     if actualLen != 0:
         elemSize = (length - 3) // actualLen
         for i in range(actualLen):
-            ret.append(decode(data[3+elemSize*i:3+elemSize*(i+1)]))
+            ret.append(decode(data[3 + elemSize * i : 3 + elemSize * (i + 1)]))
     return ret
 
-def decodeSlice(data: bytes)-> List[Any]:
+
+def decodeSlice(data: bytes) -> List[Any]:
     ret = list()
     actualLen = data[2]
-    if actualLen != 0 :
+    if actualLen != 0:
         lastEnd = 3
         for _ in range(actualLen):
-            nowEnd = lastEnd + int(data[lastEnd+1])
+            nowEnd = lastEnd + int(data[lastEnd + 1])
             ret.append(decode(data[lastEnd:nowEnd]))
             lastEnd = nowEnd
+    return ret
+
+
+def decodeStruct(data: bytes) -> List[Any]:
+    ret = dict()
+    actualLen = data[2]
+    if actualLen != 0:
+        lastEnd = 3
+        for _ in range(actualLen):
+            keyLen = data[lastEnd + 1]
+            key = decode(data[lastEnd : lastEnd + keyLen])
+            lastEnd += keyLen
+            valueLen = data[lastEnd + 1]
+            value = decode(data[lastEnd : lastEnd + valueLen])
+            lastEnd += valueLen
+            ret[key] = value
     return ret
