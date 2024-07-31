a65dd9405d2d402867614c6f5d47823bba4ce5f2
dan1elee <87975928+dan1elee@users.noreply.github.com>
2024-07-31 15:00:21 +0800

    unserialize.h-定义新的结构体和联合体，从而支持数组等其他数据结构

--- crosslang/cpp/unserialize.h
+++ crosslang/cpp/unserialize.h
@@ -5,6 +5,8 @@
 #include <complex>
 
 typedef uint8_t byte;
+typedef std::complex<float> complex64;
+typedef std::complex<double> complex128;
 
 class UnserializeException : public std::exception
 {
@@ -21,6 +23,73 @@ private:
     std::string message;
     std::string type2Str(int type) const;
 };
+
+namespace enums
+{
+    enum Types
+    {
+        NIL,
+        BOOL,
+        INT,
+        UINT,
+        INT8,
+        UINT8,
+        INT16,
+        UINT16,
+        INT32,
+        UINT32,
+        INT64,
+        UINT64,
+        FLOAT32,
+        FLOAT64,
+        COMPLEX64,
+        COMPLEX128,
+        // UINTPTR
+        ARRAY,
+        STRUCT,
+        STRING,
+        SLICE,
+        MAP,
+        PTR,
+        ENDOFTYPE
+    };
+    const int EncodeHeaderLen = 2;
+}
+
+typedef union unionval UnionVal;
+
+typedef struct
+{
+    int len;
+    UnionVal *valList;
+} Arr;
+
+typedef union unionval
+{
+    bool boolVal;
+    int8_t int8Val;
+    int16_t int16Val;
+    int32_t int32Val;
+    int64_t int64Val;
+    uint8_t uint8Val;
+    uint16_t uint16Val;
+    uint32_t uint32Val;
+    uint64_t uint64Val;
+    float float32Val;
+    double float64Val;
+    complex64 complex64Val;
+    complex128 complex128Val;
+    Arr arrVal;
+    // TODO
+} UnionVal;
+
+typedef struct
+{
+    enums::Types type;
+    UnionVal val;
+} typeAndVal;
+
+enums::Types getDataType(byte *data, int len);
 
 void unserialize(byte *data, int len, void *result);
 
@@ -41,6 +110,7 @@ uint64_t readUint64(byte *data, int len);
 float readFloat32(byte *data, int len);
 double readFloat64(byte *data, int len);
 
-std::complex<float> readComplex64(byte *data, int len);
-std::complex<double> readComplex128(byte *data, int len);
+complex64 readComplex64(byte *data, int len);
+complex128 readComplex128(byte *data, int len);
+
 #endif
